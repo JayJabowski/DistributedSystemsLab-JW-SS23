4ef489c76d377d2cf576f1494459038dd6bbd935
Jan Wittrowski <jawiit00@hs-esslingen.de>
2023-04-12 12:15:30 +0200

    added spring test

---
+++ spring_boot/testv1/src/test/java/VerteilteSysteme/tryout/testv1/Testv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package VerteilteSysteme.tryout.testv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
